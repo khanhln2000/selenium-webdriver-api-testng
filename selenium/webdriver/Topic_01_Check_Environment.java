6a7f991b681a5ecdb622977c3ed7baa7b68b5c67
KhanhLN <khanhln2000@gmail.com>
2024-07-11 17:24:24 +0700

    add new project

---
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -0,0 +1,45 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_Check_Environment {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Windows")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		}
+
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://m.facebook.com/?_rdr");
+	}
+
+	@Test
+	public void TC_01_Url() {
+		Assert.assertEquals(driver.getCurrentUrl(), "https://m.facebook.com/?_rdr");
+	}
+
+	@Test
+	public void TC_02_Title() {
+		Assert.assertEquals(driver.getTitle(),"Facebook – log in or sign up");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.close();
+	}
+}
